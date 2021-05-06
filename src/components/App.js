5b0ec1d5a9b3027c9466b2327708581b53986419
gourav <gouravthakur525@gmail.com>
2021-05-06 19:36:03 +0530

    updated code

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      <h2 className="data-ns-test" value="project-name"> Education For U </h2>
-      <p  className="data-ns-test" value="project-description"> Education For U is a Eduacational Portal which is used to Prepare for various Competative Education </p>
+      <h2 data-ns-test project-name> Education For U </h2>
+      <p  data-ns-test project-description> Education For U is a Eduacational Portal which is used to Prepare for various Competative Education </p>
     </div>
   )
 }
