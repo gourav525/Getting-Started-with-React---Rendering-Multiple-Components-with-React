719a5320115e69a4347eced4f2fc89369ed1b27c
gourav <gouravthakur525@gmail.com>
2021-05-06 13:39:46 +0530

    updated code

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h2 className="data-ns-test" value="project-name"> Education For U </h2>
+      <p  className="data-ns-test"  value="project-description"> Education For U is an Eduacational Portal which is used to Prepare for various Competative Education </p>
+    </div>
   )
 }
 
