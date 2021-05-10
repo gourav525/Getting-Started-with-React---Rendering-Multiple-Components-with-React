f1e3b9e8f2338574e142ecb528e57bc02c60636e
gourav <gouravthakur525@gmail.com>
2021-05-10 13:22:19 +0530

    updated result

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
-
-const App = () => {
-  return (
-    <div id="main">
-      <h2 data-ns-test project-name> Education For U </h2>
-      <p  data-ns-test project-description> Education For U is a Eduacational Portal which is used to Prepare for various Competative Education </p>
-    </div>
-  )
-}
-
-
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+const App = () => { 
+  
+   return (    
+   <div id="main">     
+      <h1 data-ns-test="project-name">"Education 4 U  "</h1>    
+      <p data-ns-test="project-description">"About various Competattive Examination"</p>    
+  </div>  );
+     
+  };
 export default App;
